786b063e43113f73a90cc383b53c618911424a92
Tinguaro Cubas Saiz <tinguarocubas@gmail.com>
2013-12-28 18:59:36 +0000

    Encontrar una única palabra clave

--- js/asistente.js
+++ js/asistente.js
@@ -1,5 +1,4 @@
 var final_transcript;
-var tiempo = 3000;
 // *****************************************************************************/
 // IMPORTANTE!! Esta función no rula hasta que esté en el servidor el proyecto 
 // -> USO LA ÚLTIMA COMO AUXILIAR A TRAVÉS DEL BOTON ESE DE GOOGLE PARA RECONOCIMIENTO DE VOZ!! 
@@ -37,91 +36,104 @@ function iniciarReconocimiento () {
 function obtenerOrden (frase) {
 	var ordenes = [];
 	ordenes = frase.split (" ");
+	var encontrado = false;
 	for (var i in ordenes) {
-		ordenes[i] = ordenes[i].toLowerCase ();
-		switch  (ordenes[i]) {
-			case 'beber': case 'tomar': 
-				console.log ("Entendida la palabra " + ordenes[i] + " en lo pronunciado");
-				mostrarAlertaBeber();
-				$("#textoAsistente").empty();
-				$("#textoAsistente").append($('<div class="notice marker-on-bottom bg-amber fg-white">He entendido ir a beber</div>'));
-				$("#sonidosAsistente").empty ();
-				$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=He+entendido+ir+a+beber' style='display: none'></iframe>");
-			break;
-		
-			case 'comer':
-				console.log ("Entendida la palabra  " + ordenes[i] + "  en lo pronunciado");
-				mostrarAlertaComer();
-				$("#textoAsistente").empty();
-				$("#textoAsistente").append($('<div class="notice marker-on-bottom bg-amber fg-white">He entendido ir a comer</div>'));
-				$("#sonidosAsistente").empty ();
-				$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=He+entendido+ir+a+comer' style='display: none'></iframe>");
-			break;
+		if(!encontrado) {
+			ordenes[i] = ordenes[i].toLowerCase ();
+			switch  (ordenes[i]) {
+				case 'beber': case 'tomar': 
+					console.log ("Entendida la palabra " + ordenes[i] + " en lo pronunciado");
+					mostrarAlertaBeber();
+					$("#textoAsistente").empty();
+					$("#textoAsistente").append($('<div class="notice marker-on-bottom bg-amber fg-white">He entendido ir a beber</div>'));
+					$("#sonidosAsistente").empty ();
+					$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=He+entendido+ir+a+beber' style='display: none'></iframe>");
+					encontrado = true;
+				break;
 			
-			case 'fiesta':	case 'noche':
-				console.log ("Entendida la palabra  " + ordenes[i] + "  en lo pronunciado");
-				mostrarAlertaNoche();
-				$("#textoAsistente").empty();
-				$("#textoAsistente").append($('<div class="notice marker-on-bottom bg-amber fg-white">He entendido sitios donde salir de fiesta o tomar algo</div>'));
-				$("#sonidosAsistente").empty ();
-				$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=He+entendido+sitios+donde+salir+de+fiesta+o+a+tomar+algo' style='display: none'></iframe>");
-			break;
-			
-			case 'principal': case 'inicio':
-				$("#sonidosAsistente").empty ();
-				$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Has+elegido+ir+a+inicio' style='display: none'></iframe>");		
-				$.mobile.changePage("#page1");
-			break;
-
-			case 'tenerife': case 'finder':
-				$("#sonidosAsistente").empty ();
-				$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Has+elegido+ir+a+Tenerife+Finder' style='display: none'></iframe>");		
-				$.mobile.changePage("#page2");
-			break;
-						
-			case 'acerca': case 'sobre': case 'about':
-				$("#sonidosAsistente").empty ();
-				$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Has+elegido+ir+a+nuestra+información' style='display: none'></iframe>");
-				$.mobile.changePage("#page3");
-			break;
-			
-			case 'hola':
-				$("#textoAsistente").empty();
-				$("#textoAsistente").append($('<div class="notice marker-on-bottom bg-amber fg-white">Hola &iquestQu&eacute tal?</div>'));
-				$("#sonidosAsistente").empty ();
-				$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Hola+qué+tal' style='display: none'></iframe>");
-			break;
-			
-			case 'chao': case 'adios':
-				$("#sonidosAsistente").empty ();
-				$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Adiós+Espero+volver+a+verle+pronto' style='display: none'></iframe>");
-				$.mobile.changePage("#page1");
-			break;
-			
-			case 'salir':
-				var x = window.confirm("Seguro de querer abandonar el sitio web ?")
-				if (x) {
-					window.alert("Vuelva pronto!  :)")
-					window.open('', '_self', '');
-					window.close();
-				} else {
-					
-				}
-			break;
-			
-			case 'p***': case 'tonta': case 'g*********': case 'idiota':
-				$("#textoAsistente").empty();
-				$("#textoAsistente").append($('<div class="notice marker-on-bottom bg-amber fg-white">Eso lo ser&aacute tu madre, por favor tenga educaci&oacuten</div>'));					
-				$("#sonidosAsistente").empty ();
-				$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Eso+lo+será+tu+madre+por+favor+tenga+educación' style='display: none'></iframe>");
+				case 'comer':
+					console.log ("Entendida la palabra  " + ordenes[i] + "  en lo pronunciado");
+					mostrarAlertaComer();
+					$("#textoAsistente").empty();
+					$("#textoAsistente").append($('<div class="notice marker-on-bottom bg-amber fg-white">He entendido ir a comer</div>'));
+					$("#sonidosAsistente").empty ();
+					$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=He+entendido+ir+a+comer' style='display: none'></iframe>");
+					encontrado = true;
+				break;
 				
-			break;
-			
-			case 'registro': case 'registrarse':
-				$("#sonidosAsistente").empty ();
-				$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=por+favor+rellene+los+datos+en+el+formulario+de+registro+que+le+mostramos' style='display: none'></iframe>");
-				$.mobile.changePage("#pageRegistro");
-			break;
+				case 'fiesta':	case 'noche':
+					console.log ("Entendida la palabra  " + ordenes[i] + "  en lo pronunciado");
+					mostrarAlertaNoche();
+					$("#textoAsistente").empty();
+					$("#textoAsistente").append($('<div class="notice marker-on-bottom bg-amber fg-white">He entendido sitios donde salir de fiesta o tomar algo</div>'));
+					$("#sonidosAsistente").empty ();
+					$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=He+entendido+sitios+donde+salir+de+fiesta+o+a+tomar+algo' style='display: none'></iframe>");
+					encontrado = true;
+				break;
+				
+				case 'principal': case 'inicio':
+					$("#sonidosAsistente").empty ();
+					$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Has+elegido+ir+a+inicio' style='display: none'></iframe>");		
+					$.mobile.changePage("#page1");
+					encontrado = true;
+				break;
+
+				case 'tenerife': case 'finder':
+					$("#sonidosAsistente").empty ();
+					$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Has+elegido+ir+a+Tenerife+Finder' style='display: none'></iframe>");		
+					$.mobile.changePage("#page2");
+					encontrado = true;
+				break;
+							
+				case 'acerca': case 'sobre': case 'about':
+					$("#sonidosAsistente").empty ();
+					$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Has+elegido+ir+a+nuestra+información' style='display: none'></iframe>");
+					$.mobile.changePage("#page3");
+					encontrado = true;
+				break;
+				
+				case 'hola':
+					$("#textoAsistente").empty();
+					$("#textoAsistente").append($('<div class="notice marker-on-bottom bg-amber fg-white">Hola &iquestQu&eacute tal?</div>'));
+					$("#sonidosAsistente").empty ();
+					$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Hola+qué+tal' style='display: none'></iframe>");
+					encontrado = true;
+				break;
+				
+				case 'chao': case 'adios':
+					$("#sonidosAsistente").empty ();
+					$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Adiós+Espero+volver+a+verle+pronto' style='display: none'></iframe>");
+					$.mobile.changePage("#page1");
+					encontrado = true;
+				break;
+				
+				case 'salir':
+					encontrado = true;
+					var x = window.confirm("Seguro de querer abandonar el sitio web ?")
+					if (x) {
+						window.alert("Vuelva pronto!  :)")
+						window.open('', '_self', '');
+						window.close();
+					} else {
+						
+					}
+				break;
+				
+				case 'p***': case 'tonta': case 'g*********': case 'idiota':
+					$("#textoAsistente").empty();
+					$("#textoAsistente").append($('<div class="notice marker-on-bottom bg-amber fg-white">Eso lo ser&aacute tu madre, por favor tenga educaci&oacuten</div>'));					
+					$("#sonidosAsistente").empty ();
+					$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=Eso+lo+será+tu+madre+por+favor+tenga+educación' style='display: none'></iframe>");
+					encontrado = true;
+				break;
+				
+				case 'registro': case 'registrarse':
+					$("#sonidosAsistente").empty ();
+					$("#sonidosAsistente").append ("<iframe src='http://translate.google.com/translate_tts?tl=es&q=por+favor+rellene+los+datos+en+el+formulario+de+registro+que+le+mostramos' style='display: none'></iframe>");
+					$.mobile.changePage("#pageRegistro");
+					encontrado = true;
+				break;
+			}
 		}
 	}
 }
